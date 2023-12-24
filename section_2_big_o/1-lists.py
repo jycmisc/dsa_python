4795e5a0490997805d430374511afefa5fae0af7
jycmisc <jyc.misc@outlook.com>
2023-12-23 23:18:27 -0800

    added O analysis on list

--- section_2_big_o/1-lists.py
+++ section_2_big_o/1-lists.py
@@ -1,1 +1,8 @@
 my_list = [11, 3, 23, 7]
+
+# Add or remove at the end of list is O(1)
+# Add or remove at the beginning of list is O(n)
+# Add or remove at the middle (or any other position) of list is O(n)j
+
+# Find value in list is O(n)
+# Find value by index in the list is O(1)
